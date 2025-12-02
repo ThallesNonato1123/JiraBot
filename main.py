ee62afbe0f5a2ffe0e21d05c95dbed520bfdcaef
ThallesNonato1123 <eusouthalles@gmail.com>
2025-12-02 18:32:05 -0300

    movendo para done com cli

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import argparse
 from JiraClient import jira_client
+
+def build_parser():
+    parser = argparse.ArgumentParser(description="CLI para o JiraBot")
+    parser.add_argument("-id", "--issue-id", required=True, help="ID da issue (ex: PVTE-4894)")
+    return parser
+
+if __name__ == "__main__":
+    parser = build_parser()
+    args = parser.parse_args()
+
+    issue_id = args.issue_id
+    print("Issue informada:", issue_id)
+
+    jira = jira_client
+    jira.move_to_done(issue_id)
