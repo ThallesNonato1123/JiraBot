50c6464b1784aaf84e41a3d5d7435cc55f87587b
ThallesNonato1123 <eusouthalles@gmail.com>
2025-12-02 21:56:25 -0300

    adicionado novo main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from JiraClient import jira_client
+from GithubClient import GithubClient
+import os
+
+if __name__ == "__main__":
+    github_token = os.getenv("GITHUB_TEST")
+    github_client =  GithubClient(github_token, "ThallesNonato1123", "TESTE-BITRISE", 2)
+    jira_tasks = github_client.getJirasAssociatedTasks()
+    print(jira_tasks)
+    # for task in jira_tasks:
+    #     jira_client.move_to_done(task)
