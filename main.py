d56c09b1e024fe991c06d6567575358bb185826b
ThallesNonato1123 <eusouthalles@gmail.com>
2025-12-03 17:49:34 -0300

    bot

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import os
 
 if __name__ == "__main__":
     github_token = os.getenv("GITHUB_TEST")
-    github_client =  GithubClient(github_token, "ThallesNonato1123", "TESTE-BITRISE", 2)
+    github_client =  GithubClient(github_token, "ThallesNonato1123", "TESTE-BITRISE", 1)
     jira_tasks = github_client.getJirasAssociatedTasks()
     for task in jira_tasks:
-        print(task)
         jira_client.move_to_done(task)
