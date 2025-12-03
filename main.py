6632d1c5a3893976825085bf878ee6126b572cde
ThallesNonato1123 <eusouthalles@gmail.com>
2025-12-02 21:38:22 -0300

    fim do bot

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import argparse
 from JiraClient import jira_client
 from GithubClient import GithubClient
-
-def build_parser():
-    parser = argparse.ArgumentParser(description="CLI para o JiraBot")
-    parser.add_argument("-id", "--issue-id", required=True, help="ID da issue (ex: PVTE-4894)")
-    return parser
 
 if __name__ == "__main__":
     github_client =  GithubClient("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", "ThallesNonato1123", "TESTE-BITRISE", 2)
