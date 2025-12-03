2e8e576ecf8db499e1905c8a51fb885bda5b9156
ThallesNonato1123 <eusouthalles@gmail.com>
2025-12-03 17:47:58 -0300

    bot

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     github_client =  GithubClient(github_token, "ThallesNonato1123", "TESTE-BITRISE", 2)
     jira_tasks = github_client.getJirasAssociatedTasks()
     for task in jira_tasks:
+        print(task)
         jira_client.move_to_done(task)
